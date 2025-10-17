7d0db4487a283eb266491ee3a07f8287b555f635
bout-tech <docuvuxuro19@gamil.com>
2025-10-18 05:02:44 +0900

    remove rainbow module

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,6 @@
 import { createRoot } from 'react-dom/client'
 import { WagmiProvider } from 'wagmi';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
-import { RainbowKitProvider, darkTheme } from '@rainbow-me/rainbowkit';
 import '@rainbow-me/rainbowkit/styles.css';
 
 import App from './App.tsx'
@@ -9,19 +8,11 @@ import './index.css'
 import { config } from './utils/wagmi.ts';
 
 const queryClient = new QueryClient();
-const myCustomTheme = darkTheme({
-  borderRadius: "medium",  // Keep UI compact
-  overlayBlur: "small",    // Reduce modal size effect
-  fontStack: "system",     // Use system font for better scaling
-  // accentColor: "#ff007a",  // Customize color (optional)
-});
 
 createRoot(document.getElementById('root')!).render(
   <WagmiProvider config={config}>
     <QueryClientProvider client={queryClient}>
-      <RainbowKitProvider theme={myCustomTheme}>
-        <App />
-      </RainbowKitProvider>
+      <App />
     </QueryClientProvider>
   </WagmiProvider >
 )
